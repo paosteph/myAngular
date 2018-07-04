36c8b2906e504e48bbf5da48d97bb424ab921f5e
Pao <pao_st55@hotmail.com>
2018-07-04 10:52:35 -0500

    segmentos

--- Rutas2/src/app/app.component.ts
+++ Rutas2/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private _router: Router){
+
+  }
+
+  irAPerfilDeUsuario(){
+    const url = ['/home','usuario',true,'u',5]; //segemntos
+    this._router.navigate(url);
+  }
 }
